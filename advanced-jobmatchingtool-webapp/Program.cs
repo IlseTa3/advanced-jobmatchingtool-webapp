e2347783be2e0daec7e2f8ea5c707c7ae056738c
Ilse <ilse.tastenhoye@gmail.com>
2026-03-13 13:06:04 +0100

    migration for db added

--- advanced-jobmatchingtool-webapp/Program.cs
+++ advanced-jobmatchingtool-webapp/Program.cs
@@ -70,7 +70,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
